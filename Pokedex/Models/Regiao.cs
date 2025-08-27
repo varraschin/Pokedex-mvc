a08d7c04dce15d74a1d286aded06645061d3df37
varraschin <marcelojuniber@gmail.com>
2025-08-26 22:32:34 -0300

    Criado o projeto e a camada de dados

---
+++ Pokedex/Models/Regiao.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pokedex.Models;
+
+[Table("Regiao")]
+public class Regiao
+{
+    [Key]
+    public uint Id { get; set; }
+
+    [StringLength(30)]
+    [Required(ErrorMessage = "Por favor, insira o nome: ")]
+    public string Nome { get; set; }
+}
---
+++ Pokedex/Models/Tipo.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Pokedex.Models;
+
+[Table("Tipo")]
+public class Tipo
+{
+    [Key]
+    public uint Id { get; set; }
+
+    [StringLength(30)]
+    [Required(ErrorMessage = "Por favor, informe o nome")]
+    public string Nome { get; set; }
+
+    [StringLength(25)]
+    public string Cor { get; set; } = "#000";
+}
